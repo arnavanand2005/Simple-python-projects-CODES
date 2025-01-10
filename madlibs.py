2bbe348b0fe10b75e8042ae322563119a6141bfc
Arnav Anand <arnavanand2005@gmail.com>
2025-01-11 03:43:40 +0530

    Update madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -1,13 +1,14 @@
 adjective=input("ENTER AN ADJECTIVE (description) : ")
-adjective1=input("ENTER AN ADJECTIVE (description) : ")
-adjective2=input("ENTER AN ADJECTIVE (description) : ")
-adjective3=input("ENTER AN ADJECTIVE (description) : ")
 name=input("ENTER A NAME : ")
-emotion=input("ENTER AN EMOTION : ")
-verb=input("ENTER A VERB (basically ending with an 'ing) : ")
+verb=input("ENTER A VERB (not ending with an 'ing') : ")
 noun=input("ENTER A NOUN (name/place/thing) : ")
+adjective1=input("ENTER AN ADJECTIVE (description) : ")
+emotion=input("ENTER AN EMOTION : ")
 noun1=input("ENTER A NOUN (name/place/thing) : ")
+adjective2=input("ENTER AN ADJECTIVE (description) : ")
 noun2=input("ENTER A NOUN (name/place/thing) : ")
+verb2=input("ENTER A VERB (basically ending with an 'ing') : ")
+adjective3=input("ENTER AN ADJECTIVE (description) : ")
 
 
 storymessage=(f"""One day, a {adjective} cat named {name} tried to {verb} a {noun}. 
@@ -15,6 +16,6 @@ It was {adjective1} and totally {emotion}.
 But as soon as it touched the {noun1},
 a {adjective2} dog jumped out and shouted,
 Hey! That's my {noun2}!' The cat froze,
-then {verb} away leaving the dog to chase after it in a {adjective3} frenzy.""")
+then {verb2} away leaving the dog to chase after it in a {adjective3} frenzy.""")
 
 print(storymessage)
