b91260d0a2c0f9d4ae0af9d0758296f5f60723d9
Arnav Anand <arnavanand2005@gmail.com>
2025-01-11 03:40:40 +0530

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,20 @@
+adjective=input("ENTER AN ADJECTIVE (description) : ")
+adjective1=input("ENTER AN ADJECTIVE (description) : ")
+adjective2=input("ENTER AN ADJECTIVE (description) : ")
+adjective3=input("ENTER AN ADJECTIVE (description) : ")
+name=input("ENTER A NAME : ")
+emotion=input("ENTER AN EMOTION : ")
+verb=input("ENTER A VERB (basically ending with an 'ing) : ")
+noun=input("ENTER A NOUN (name/place/thing) : ")
+noun1=input("ENTER A NOUN (name/place/thing) : ")
+noun2=input("ENTER A NOUN (name/place/thing) : ")
+
+
+storymessage=(f"""One day, a {adjective} cat named {name} tried to {verb} a {noun}. 
+It was {adjective1} and totally {emotion}.
+But as soon as it touched the {noun1},
+a {adjective2} dog jumped out and shouted,
+Hey! That's my {noun2}!' The cat froze,
+then {verb} away leaving the dog to chase after it in a {adjective3} frenzy.""")
+
+print(storymessage)
